1c8e304ee1196eff3ae50416e20d18d8e4ad632d
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-10-01 02:20:22 +0530

    added 2 files with violations

---
+++ data_utils.js
@@ -0,0 +1,50 @@
+class data_validator {
+  #_validationRules = {};
+
+  constructor(rules_object) {
+    this.#_validationRules = rules_object;
+  }
+
+  validateData(d) {
+    let Is_Valid = true;
+    for (const rule in this.#_validationRules) {
+      if (!d.hasOwnProperty(rule)) {
+        Is_Valid = false;
+        break;
+      }
+    }
+    return Is_Valid;
+  }
+}
+
+function PROCESS_ARRAY(Input_Array) {
+  let R = [];
+  for (const V of Input_Array) {
+    const v_doubled = V * 2;
+    R.push(v_doubled);
+  }
+  return R;
+}
+
+const my_utils = {
+  get_average: (data_list) => {
+    if (data_list.length === 0) return 0;
+    const TOTAL = data_list.reduce((acc, val) => acc + val, 0);
+    return TOTAL / data_list.length;
+  },
+  convert_to_string_array: (in_array) => {
+    let final_array = [];
+    in_array.forEach((element) => {
+      final_array.push(String(element));
+    });
+    return final_array;
+  }
+};
+
+const processor = new data_validator({
+  name: 'string',
+  age: 'number'
+});
+
+console.log(processor.validateData({ name: 'John', age: 30 }));
+console.log(my_utils.get_average([10, 20, 30]));
