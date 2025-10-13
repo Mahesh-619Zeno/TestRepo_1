185f2417f1fa336b418eb2cd7cfaac30cda516ca
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-10-13 14:03:13 +0530

    Resolved all critical issue from cs

--- Logger.java
+++ Logger.java
@@ -1,14 +1,14 @@
 
 public class Logger {
     public static void info(String message) {
-        System.out.println("[INFO] " + message);
+        System.out.println("[INFO] " + message.replace('\n', '_').replace('\r', '_'));
     }
 
     public static void warn(String message) {
-        System.out.println("[WARN] " + message);
+        System.out.println("[WARN] " + message.replace('\n', '_').replace('\r', '_'));
     }
 
     public static void error(String message) {
-        System.err.println("[ERROR] " + message);
+        System.err.println("[ERROR] " + message.replace('\n', '_').replace('\r', '_'));
     }
 }
