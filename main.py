b8a2c7a2dbb880b05ea8b894a0e33eb294cf4885
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-11-12 09:28:28 +0530

    Test Report - 007

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Sample flow for iteration 1
+from tasks.cli_handler import run_cli
 from tasks.task import Task, TaskManager
 from tasks.status import update_status
 from tasks.search import search_by_title, search_by_priority
@@ -7,8 +7,9 @@ print("testing")
 def main():
     manager = TaskManager()
     print("=== Welcome to Task Manager ===")
+    run_cli(manager)
 
-    
+
     # Add sample tasks
     manager.add_task(Task("Finish Report", "Complete the financial report", "High"))
     manager.add_task(Task("Email Client", "Send project updates", "Medium"))
