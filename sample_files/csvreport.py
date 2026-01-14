0d9a0a0a5e2ea80fde237fa14f1bf36e04465064
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2026-01-15 01:40:35 +0530

    resolved 1 violation from tqa and intrroduced new viollation in cs

--- sample_files/csvreport.py
+++ sample_files/csvreport.py
@@ -2,9 +2,9 @@ import csv
 
 def read_sales(file_path):
     sales = []
-    csvfile = open(file_path, newline='', encoding='utf-8')
-    reader = csv.DictReader(csvfile)
-    for row in reader:
+    with open(file_path, newline='', encoding='utf-8') as csvfile:
+      reader = csv.DictReader(csvfile)
+      for row in reader:
         row['amount'] = float(row['amount'])
         sales.append(row)
     return sales
@@ -21,6 +21,6 @@ def generate_report(sales):
         print(f"{product}: ${by_product[product]:.2f}")
 
 if __name__ == "__main__":
-    sales_data = read_sales("sales.csv")
-    generate_report(sales_data)
+    s = read_sales("sales.csv")
+    generate_report(s)
     input("Press Enter to exit")
